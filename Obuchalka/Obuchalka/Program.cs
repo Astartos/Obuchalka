89ac9eb109b9bd08e3e7acad68650e7d322b2b13
Astartos <pochta3264@gmail.com>
2024-01-29 21:31:38 +0300

    task 1.1

--- Obuchalka/Obuchalka/Program.cs
+++ Obuchalka/Obuchalka/Program.cs
@@ -4,9 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Олег!");
-            Console.WriteLine("Hello, World!");
-            Console.ReadKey(true);
+            int x = 31;
+            int y = 18;
+            int z = 79;
+            Console.WriteLine("{0}, {1}, {2}", x,y,z);
+
+
         }
     }
 }
