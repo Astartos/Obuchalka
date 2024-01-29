10997ddc880a44290acaf35f223c632590f0d1cf
Astartos <pochta3264@gmail.com>
2024-01-29 20:58:39 +0300

    поменял Илья на world

--- Obuchalka/Obuchalka/Program.cs
+++ Obuchalka/Obuchalka/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Илья!");
+            Console.WriteLine("Hello, World!");
             Console.ReadKey(true);
         }
     }
