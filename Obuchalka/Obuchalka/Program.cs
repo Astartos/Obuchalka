0c1f153f628b28d7609e3746f20d5b467c24a75d
Astartos <pochta3264@gmail.com>
2024-01-31 19:16:14 +0300

    task 1.4

--- Obuchalka/Obuchalka/Program.cs
+++ Obuchalka/Obuchalka/Program.cs
@@ -12,12 +12,21 @@
                 Console.WriteLine("{0}, {1}, {2}", x, y, z);
             }
 
-            //task 2.2
+            //task 1.2
             {
                 int x = 47;
                 int y = 52;
                 int z = 150;
                 Console.WriteLine($"{x},  {y},  {z}");
+            }
+
+            //task 1.3
+            {
+                Console.WriteLine("50\n10");
+            }
+            //task 1.4
+            {
+                Console.WriteLine("5\n10\n21");
             }
         }
     }
